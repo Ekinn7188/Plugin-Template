ae30007d8afe1866fa33da2c873a325c7bb73f89
ekinn7188 <ekinn7188@gmail.com>
2022-05-30 04:16:16 -0700

    Initial Commit

--- src/main/resources/db/migration/V1__PluginTemplate.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-                     UserID              INTEGER       PRIMARY KEY     AUTOINCREMENT,
-                     UserUUID            VARCHAR(36)   NOT NULL        UNIQUE
-);
-
